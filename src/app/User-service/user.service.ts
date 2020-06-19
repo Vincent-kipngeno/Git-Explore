def6da905399e3b1b131ef6f1cc9d263b35ef333
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-19 19:35:36 +0300

    assign parameters to user constructor and import it to user service

--- src/app/User-service/user.service.ts
+++ src/app/User-service/user.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import {environment } from '../../environments/environment';
+import { User } from './user'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  user: User;
+  constructor(private http:HttpClientModule) {
+    this.user = new User("", "", "", "")
+   }
 }
+https://api.github.com/users/daneden?access_token=85587e4d58e56bc92d46526d6c53161828bc96cc
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,4 @@
 export class User {
+
+  constructor(public login:string, public avatar:string, public public_gists:number, public public_repos:number){}
 }
