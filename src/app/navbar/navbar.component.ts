60f8e046b7d6c0333dc95c95848db486687efabe
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-22 17:11:31 +0300

    add a function to nav-bar component that allows navbar to collapse in small screens

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, AfterViewInit, Output, EventEmitter, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  navbarOpen = false;
+  public clicked = false;
+  _el: any;
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
-
+  constructor(private router: Router,) {}
+  ngOnInit() {}
+  onClick(event): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.clicked = true;
+  }
+  /*@HostListener(‘document:click’, [‘event’])
+  private clickedOutside(event): void {
+    if (this.clicked) {
+      this._el.nativeElement.querySelector(‘.dropdown-menu’).classList.toggle(‘show’);
+    }
+  }*/
 }
