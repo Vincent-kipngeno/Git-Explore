97d97e27183a487cd0953605f420a46178825ddf
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-22 17:16:58 +0300

    removed unnecessary imports from navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,30 +1,21 @@
-import { Component, OnInit, ViewEncapsulation, AfterViewInit, Output, EventEmitter, HostListener } from '@angular/core';
+import { Component, OnInit, EventEmitter, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
   navbarOpen = false;
-  public clicked = false;
-  _el: any;
+  
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
   constructor(private router: Router,) {}
-  ngOnInit() {}
-  onClick(event): void {
-    event.preventDefault();
-    event.stopPropagation();
-    this.clicked = true;
+
+  ngOnInit() {
+
   }
-  /*@HostListener(‘document:click’, [‘event’])
-  private clickedOutside(event): void {
-    if (this.clicked) {
-      this._el.nativeElement.querySelector(‘.dropdown-menu’).classList.toggle(‘show’);
-    }
-  }*/
+
 }
