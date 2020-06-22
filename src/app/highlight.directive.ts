e283792b1ac0cbfc5c6aad88335271269692bf96
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-22 11:30:11 +0300

    Add code to highlight directive that highlights repo-name on hover, increase the font-size of repo-name and add appHighlight selector in landing-page.html

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('orange');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.elem.nativeElement.style.backgroundColor = color;
+  }
+
 
 }
