c467457d461d3304cbd47b8ca1277449cd908c57
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-22 12:55:46 +0300

    add header to landing-page.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { GithubUsersComponent } from './github-users/github-users.component';
 import { GithubUsersReposComponent } from './github-users-repos/github-users-repos.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'landing', component: LandingPageComponent},
   { path: 'landing/:username', component: GithubUsersReposComponent},
   { path: 'users', component: GithubUsersComponent},
-  { path: 'repos', component: RepositoriesComponent}
+  { path: 'repos', component: RepositoriesComponent},
+  { path: '', redirectTo:"/landing", pathMatch:"full"},
+  { path:'**', component:NotFoundComponent},
 ];
 
 @NgModule({
